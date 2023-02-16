4628dc674367a94257b146ea22d32ec6c0e27c6e
kate-rzk <razkovaekaterina@gmail.com>
2023-02-17 02:55:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = 
+function reverse(n) {
+    let arr = n.toString().split('');
+    if (arr[0] == '-') {
+        return arr.slice(1, arr.length).reverse().join('')}
+    else {
+        return arr.reverse().join('')}
 }
+
